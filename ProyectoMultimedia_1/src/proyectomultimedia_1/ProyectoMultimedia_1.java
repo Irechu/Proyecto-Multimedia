7670485dc5e68ba66b814edf495df30fbaba0dfc
Irene Maria Padilla Munoz <impm0008@red.ujaen.es>
2020-03-21 13:04:44 +0100

    arreglo directorio

---
+++ ProyectoMultimedia_1/src/proyectomultimedia_1/ProyectoMultimedia_1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectomultimedia_1;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Irene Maria Padilla Munoz
+ */
+public class ProyectoMultimedia_1 extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+
+        int idIdioma = reproductorController.leeIdioma();
+        ResourceBundle resources = null;
+        if(idIdioma == 0){
+            resources = ResourceBundle.getBundle("languages.text_es");
+        }else{
+            resources = ResourceBundle.getBundle("languages.text_en");
+        }
+        Parent root = FXMLLoader.load(getClass().getResource("reproductor.fxml"), resources);
+
+        Scene scene = new Scene(root);
+        
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
