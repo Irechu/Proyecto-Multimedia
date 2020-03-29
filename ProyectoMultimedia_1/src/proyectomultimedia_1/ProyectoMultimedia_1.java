cccda10f450b4a89aefc312a30def50363c0e619
Sergio Bonachera Romero <sbr00017@red.ujaen.es>
2020-03-29 21:43:06 +0200

    Añadida ruta relativa para los favoritos

--- ProyectoMultimedia_1/src/proyectomultimedia_1/ProyectoMultimedia_1.java
+++ ProyectoMultimedia_1/src/proyectomultimedia_1/ProyectoMultimedia_1.java
@@ -36,7 +36,7 @@ public class ProyectoMultimedia_1 extends Application {
         preferences.putInt("idIdioma", 0);
         preferences.putInt("tab", PLAYER);
         preferences.put("library", "");
-        preferences.put("favouritePath", "G:/Proyecto-Multimedia/ProyectoMultimedia_1/src/assets/favourites.txt");
+        preferences.put("favouritePath", "./src/assets/favourites.txt");
          */
         int idIdioma = preferences.getInt("idIdioma", 0);
         ResourceBundle resources = null;
